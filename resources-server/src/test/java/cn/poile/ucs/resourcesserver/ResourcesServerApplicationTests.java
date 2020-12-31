6d6282164240a77ea5b6444beb724d1c17f91681
Juminiy <2285151098@qq.com>
2020-12-31 17:48:27 +0800

    git init first

---
+++ resources-server/src/test/java/cn/poile/ucs/resourcesserver/ResourcesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.poile.ucs.resourcesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
